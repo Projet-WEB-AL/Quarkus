58e434dcf82202789255b70ba5f9d1f689a922d4
Payll <yann.paillard@laposte.net>
2023-01-07 11:32:13 +0100

    add mailler

--- src/main/java/org/acme/RmqConsumer.java
+++ src/main/java/org/acme/RmqConsumer.java
@@ -1,12 +1,19 @@
 package org.acme;
 
+import com.oracle.svm.core.annotate.Inject;
 import io.quarkus.logging.Log;
+import io.quarkus.mailer.Mail;
+import io.quarkus.mailer.Mailer;
 import org.eclipse.microprofile.reactive.messaging.Incoming;
 
 public class RmqConsumer {
 
-    @Incoming("my-queue")
+    @Inject
+    Mailer mailer;
+
+    @Incoming("msg")
     public void consume(String data) {
         Log.info("Received data: " + data);
+        mailer.send(Mail.withText("maildevt3", "A simple email from quarkus", data));
     }
 }
